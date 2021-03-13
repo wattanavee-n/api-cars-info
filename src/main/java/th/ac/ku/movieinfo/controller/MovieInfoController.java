838d199d11e28e4e5b4f47c1b13f9388733092a3
wattanavee-n <wattanavee.n@ku.th>
2021-03-13 22:15:00 +0700

    Commit Code Update And Delete Function Branch main

--- src/main/java/th/ac/ku/movieinfo/controller/MovieInfoController.java
+++ src/main/java/th/ac/ku/movieinfo/controller/MovieInfoController.java
@@ -40,4 +40,45 @@ public class MovieInfoController {
         return MovieInfoResponse;
     }
 
+    @PutMapping("/update/{id}")
+    public MovieInfoResponse update(@PathVariable int id,
+                                           @RequestBody MovieInfoEntity movieInfoEntity) {
+        MovieInfoResponse movieInfoResponse = new MovieInfoResponse();
+        try {
+            MovieInfoEntity movieInfo = this.movieInfoRepository.findById(id);
+            if (null != movieInfo) {
+                movieInfo.setName(movieInfoEntity.getName());
+                movieInfo.setdesc(movieInfoEntity.getdesc());
+                movieInfo.setMovieTheater(movieInfoEntity.getMovieTheater());
+                movieInfo.setStartDate(movieInfoEntity.getStartDate());
+                movieInfo.setShowTimes(movieInfoEntity.getShowTimes());
+                movieInfo = this.movieInfoRepository.save(movieInfo);
+                movieInfo.setMovieInformation(movieInfo);
+                movieInfo.setMessage("Update Success");
+            } else {
+                movieInfoResponse.setMessage("Everything all up to date");
+            }
+        } catch (Exception exception) {
+            movieInfoResponse.setMessage("Can not update");
+        }
+        return movieInfoResponse;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public MovieInfoResponse update(@PathVariable int id) {
+        MovieInfoResponse movieInfoResponse = new MovieInfoResponse();
+        try {
+            MovieInfoEntity movieInfo = this.movieInfoRepository.findById(id);
+            if (null != movieInfo) {
+                this.movieInfoRepository.delete(movieInfo);
+                movieInfoResponse.setMessage("Delete Success");
+            } else {
+                movieInfoResponse.setMessage("No data to delete");
+            }
+        } catch (Exception exception) {
+            movieInfoResponse.setMessage("Can not Delete");
+        }
+        return movieInfoResponse;
+    }
+
 }
